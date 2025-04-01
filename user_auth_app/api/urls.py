c6d2251c7f4e8ce26d4c6c1b0c8ca291bc8661e2
Rene-Heller <rene-heller@developerakademie.com>
2025-04-01 08:15:48 +0200

    getting auth token through login

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import UserProfileList,UserProfileDetailAccess
+from .views import UserProfileList,UserProfileDetailAccess, RegistrationView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name= 'userprofile-list'),
-    path('profiles/<int:pk>/', UserProfileDetailAccess.as_view(), name = 'userprofile-detail')
+    path('profiles/<int:pk>/', UserProfileDetailAccess.as_view(), name = 'userprofile-detail'),
+    path('registration/', RegistrationView.as_view(), name = 'registration'),
+    path('login/', obtain_auth_token, name='login')
 ]
