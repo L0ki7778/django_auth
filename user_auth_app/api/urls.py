62151f1b21ba0839500ee3dcffc39ce680cbcdab
Rene-Heller <rene-heller@developerakademie.com>
2025-04-01 08:29:06 +0200

    added custom login to get auth token

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserProfileList,UserProfileDetailAccess, RegistrationView
+from .views import UserProfileList,UserProfileDetailAccess, RegistrationView,CustomLoginView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name= 'userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetailAccess.as_view(), name = 'userprofile-detail'),
     path('registration/', RegistrationView.as_view(), name = 'registration'),
-    path('login/', obtain_auth_token, name='login')
+    path('login/', CustomLoginView.as_view(), name='login')
 ]
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -4,7 +4,9 @@ from .serializers import UserProfileSerializer, RegistrationSerializers
 from user_auth_app.models import UserProfile
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+
 
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
@@ -13,6 +15,25 @@ class UserProfileList(generics.ListCreateAPIView):
 class UserProfileDetailAccess(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+    def post(self,request):
+        serializer = self.serializer_class(data = request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token = Token.objects.get(user = user)
+            data = {
+                'token':token.key,
+                'username':user.username,
+                'email': user.email
+            }
+        else:
+            data= serializer.errors
+        return Response(data)
+
+
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
